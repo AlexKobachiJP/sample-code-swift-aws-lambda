03601f24f94181ab8718ae3e1181aae9ca497b8b
Alex Kovács <alex@kobachi.jp>
2023-02-01 18:52:33 +0900

    Add dependency

--- Package.swift
+++ Package.swift
@@ -11,13 +11,15 @@ let package = Package(
         .executable(name: "webfinger", targets: ["WebFinger"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-server/swift-aws-lambda-runtime.git", from: "1.0.0-alpha")
+        .package(url: "https://github.com/swift-server/swift-aws-lambda-runtime", from: "1.0.0-alpha"),
+        .package(url: "https://github.com/swift-server/swift-aws-lambda-events", branch: "main")
     ],
     targets: [
         .executableTarget(
             name: "WebFinger",
             dependencies: [
                 .product(name: "AWSLambdaRuntime", package: "swift-aws-lambda-runtime"),
+                .product(name: "AWSLambdaEvents", package: "swift-aws-lambda-runtime")
             ]
         ),
     ]
