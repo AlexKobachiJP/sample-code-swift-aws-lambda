63d030186875b2ca7af42bf4b169418c64fcee0c
Alex Kovács <alex@kobachi.jp>
2023-02-02 09:53:24 +0900

    Add documenation

--- Sources/WebFinger/WebFingerHandler.swift
+++ Sources/WebFinger/WebFingerHandler.swift
@@ -5,6 +5,16 @@ import AWSLambdaEvents
 
 @main
 struct WebFingerHandler: SimpleLambdaHandler {
+
+    /// Handle request with query string: `"resource=acct:<account>"`.
+    ///
+    /// This handler assumes it will be called from a service that translates HTTP requests into events of type`APIGatewayV2Request`
+    /// and expects response objects of type `APIGatewayV2Response`.  This is true for AWS API Gateway, CloudFront or when calling the
+    /// Lambda function via its function URL.
+    /// - Parameters:
+    ///   - event: An `APIGatewayV2Request` with appropriate `queryStringParameters`.
+    ///   - context: A `LambdaContext` that can be used, for example, to obtain a `logger`.
+    /// - Returns: An `APIGatewayV2Response` with the response body expected by WebFinger.
     func handle(_ event: APIGatewayV2Request, context: LambdaContext) async throws -> APIGatewayV2Response {
         logEventIfNecessary(event, context: context)
         
